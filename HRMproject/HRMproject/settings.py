7e0f0c18c77a85676c7b2a4920308d2935ddaa21
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-08-17 16:31:12 +0700

    test deploy 34

--- HRMproject/HRMproject/settings.py
+++ HRMproject/HRMproject/settings.py
@@ -20,7 +20,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
-
+ENV = os.getenv("ENV", "development")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -40,25 +40,46 @@ CORS_ALLOWED_ORIGINS = [
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'oauth2_provider',
-    'django_mysql',
-    'drf_yasg',
-    'user',
-    'employee',
-    'timesheet',
-    'facerecognition',
-    'corsheaders',
-    'salary',
-    "channels",
-    "worklocation",
-]
+if ENV == "production": 
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        "django.contrib.postgres",
+        'oauth2_provider',
+        'drf_yasg',
+        'user',
+        'employee',
+        'timesheet',
+        'facerecognition',
+        'corsheaders',
+        'salary',
+        "channels",
+        "worklocation",
+    ]
+else:
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'oauth2_provider',
+        'django_mysql',
+        'drf_yasg',
+        'user',
+        'employee',
+        'timesheet',
+        'facerecognition',
+        'corsheaders',
+        'salary',
+        "channels",
+        "worklocation",
+    ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -116,7 +137,7 @@ CHANNEL_LAYERS = {
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-ENV = os.getenv("ENV", "development")
+
 
 if ENV == "production":
     DATABASES = {
